19c69f1b1bf7e9f2b5164411bfa5ca1cf110f6ed
maheshmohan8089 <maheshmohan8089@gmail.com>
2022-03-24 11:36:08 +0530

    Add project files.

---
+++ MVCSession2/Controllers/CustomerMastersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCSession2.Models;
+
+namespace MVCSession2.Controllers
+{
+    public class CustomerMastersController : Controller
+    {
+        private MVCDBEntities db = new MVCDBEntities();
+
+        // GET: CustomerMasters
+        public ActionResult Index()
+        {
+            return View(db.CustomerMaster.ToList());
+        }
+
+        // GET: CustomerMasters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerMaster customerMaster = db.CustomerMaster.Find(id);
+            if (customerMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerMaster);
+        }
+
+        // GET: CustomerMasters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerMasters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Customer_Id,Customer_Name,Address,State,PostCode")] CustomerMaster customerMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerMaster.Add(customerMaster);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customerMaster);
+        }
+
+        // GET: CustomerMasters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerMaster customerMaster = db.CustomerMaster.Find(id);
+            if (customerMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerMaster);
+        }
+
+        // POST: CustomerMasters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Customer_Id,Customer_Name,Address,State,PostCode")] CustomerMaster customerMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerMaster).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customerMaster);
+        }
+
+        // GET: CustomerMasters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerMaster customerMaster = db.CustomerMaster.Find(id);
+            if (customerMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerMaster);
+        }
+
+        // POST: CustomerMasters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CustomerMaster customerMaster = db.CustomerMaster.Find(id);
+            db.CustomerMaster.Remove(customerMaster);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCSession2/Controllers/TempDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSession2.Controllers
+{
+    public class TempDataController : Controller
+    {
+        // GET: TempData
+        public ActionResult Index()
+        {
+
+            TempData["Name"] = "Ajay";
+            TempData["Code"] = "1001";
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            if (TempData.ContainsKey("Name"))
+            {
+                var _name= TempData["Name"] as string;
+                ViewBag.FullName = _name + "Dev";
+
+            }
+            if (TempData.ContainsKey("Code"))
+            {
+                ViewBag.Code = TempData["Code"] as string;
+
+            }
+             
+           
+            return View();
+        }
+    }
+};
